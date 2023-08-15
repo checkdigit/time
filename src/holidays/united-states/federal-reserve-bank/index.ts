9bf0d73cad72dbe08d3dffe1bf14092c082363ce
Carl Ansley <carl.ansley@gmail.com>
2023-08-15 14:03:24 -0400

    Fix esbuild explicit type issue

--- src/holidays/united-states/federal-reserve-bank/index.ts
+++ src/holidays/united-states/federal-reserve-bank/index.ts
@@ -3,4 +3,4 @@
 export { default as addUSFederalReserveBankBusinessDays } from './add-us-federal-reserve-bank-business-days';
 export { default as getAllUSFederalReserveBankHolidays } from './get-all-us-federal-reserve-bank-holidays';
 export { default as isUSFederalReserveBankClosed } from './is-us-federal-reserve-bank-closed';
-export { PlainDate } from './plain-date';
+export { type PlainDate } from './plain-date';
