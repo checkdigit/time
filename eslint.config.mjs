ca8db16d754046d7c7949e64757ef957adf1a05d
Le Cong <le.cong@checkdigit.com>
2025-02-20 22:28:16 -0500

    cleanup eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,8 +5,6 @@ export default [
   {
     rules: {
       '@checkdigit/file-path-comment': 'off',
-      '@checkdigit/no-side-effects': 'off',
-      '@checkdigit/require-assert-predicate-rejects-throws': 'off',
       'markdown/no-missing-label-refs': 'off',
       'markdown/fenced-code-language': 'off',
       'unicorn/filename-case': 'off',
